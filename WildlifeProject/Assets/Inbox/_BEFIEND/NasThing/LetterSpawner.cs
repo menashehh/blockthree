0843d6dd5ddc5ff74c1fe2cf1dfab2a67bd56592
menashehh <veraholwerda@live.com>
2023-06-11 23:40:37 +0200

    Letters created
    I made letters for 'Welcome' and two Events. The script still only spawns the first somehow, and there's a (non-disruptive?) Event issue.

--- WildlifeProject/Assets/Inbox/_BEFIEND/NasThing/LetterSpawner.cs
+++ WildlifeProject/Assets/Inbox/_BEFIEND/NasThing/LetterSpawner.cs
@@ -15,9 +15,19 @@ public class LetterSpawner : MonoBehaviour
         InvokeRepeating(nameof(SpawnLetter), 0f, secondsInBetweenSpawns);
     }
 
+    private int letterSpawnCount = 0;
+
     private void SpawnLetter()
     {
+        if (letterSpawnCount >= 3)
+        {
+            CancelInvoke(nameof(SpawnLetter));
+            return;
+        }
+
         LetterContent randomLetter = allLetters[Random.Range(0, allLetters.Count)];
         mailboxManager.ReceiveNewLetter(randomLetter);
+
+        letterSpawnCount++;
     }
 }
