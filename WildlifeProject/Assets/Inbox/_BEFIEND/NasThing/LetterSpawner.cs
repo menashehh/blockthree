16750b20d3a9a7d086d5b24d2c5d7065115523fd
ComeOnProTM <101826779+ComeOnProTM@users.noreply.github.com>
2023-06-14 21:58:16 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/menashehh/blockthree""
    This reverts commit 40cc85e76dbb748eff8ecb981b41193f9e395a45.

--- WildlifeProject/Assets/Inbox/_BEFIEND/NasThing/LetterSpawner.cs
+++ WildlifeProject/Assets/Inbox/_BEFIEND/NasThing/LetterSpawner.cs
@@ -10,24 +10,40 @@ public class LetterSpawner : MonoBehaviour
     [SerializeField] private int secondsInBetweenSpawns;
     [SerializeField] private List<LetterContent> allLetters;
 
+    private int letterIndex = 0;
+    
     private void Awake()
     {
         InvokeRepeating(nameof(SpawnLetter), 0f, secondsInBetweenSpawns);
     }
 
-    private int letterSpawnCount = 0;
-
     private void SpawnLetter()
     {
-        if (letterSpawnCount >= 3)
+        if (letterIndex >= allLetters.Count)
         {
             CancelInvoke(nameof(SpawnLetter));
             return;
         }
 
-        LetterContent randomLetter = allLetters[Random.Range(0, allLetters.Count)];
-        mailboxManager.ReceiveNewLetter(randomLetter);
+        LetterContent letterToSpawn = allLetters[letterIndex];
+        mailboxManager.ReceiveNewLetter(letterToSpawn);
 
-        letterSpawnCount++;
+        letterIndex++;
     }
+
+    /**    private int letterSpawnCount = 0;
+
+        private void SpawnLetter()
+        {
+            if (letterSpawnCount >= 3)
+            {
+                CancelInvoke(nameof(SpawnLetter));
+                return;
+            }
+
+            LetterContent randomLetter = allLetters[Random.Range(0, allLetters.Count)];
+            mailboxManager.ReceiveNewLetter(randomLetter);
+
+            letterSpawnCount++;
+        }**/
 }
